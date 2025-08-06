be8abd0088028e4589d45e0c584d8f7940798bce
Vahid Sharifi <s4217641@glos.ac.uk>
2025-08-06 20:15:17 +0100

    Update server.js

--- webapp/server.js
+++ webapp/server.js
@@ -229,7 +229,7 @@ function runConvertScript(inputFile, outputFile) {
 }
 
 // --- Start Server ---
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Webapp server listening on port ${PORT}`);
     console.log(`Access the app at http://localhost:${PORT}`);
